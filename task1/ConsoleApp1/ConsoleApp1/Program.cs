617566a58d0fae634db90501114cf2b13819fced
Manzurul-1998 <81577924+Manzurul-1998@users.noreply.github.com>
2025-10-29 18:51:20 +0600

    Add files via upload

---
+++ task1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter score for Student 1: ");
+        double score1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter score for Student 2: ");
+        double score2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter score for Student 3: ");
+        double score3 = Convert.ToDouble(Console.ReadLine());
+
+        double average = (score1 + score2 + score3) / 3;
+        Console.WriteLine($"Average score: {average:F2}");
+    }
+}
---
+++ task2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double originalPrice = 1500;
+        double discountRate = 0.10; // 10% discount
+
+        double discountAmount = originalPrice * discountRate;
+        double finalPrice = originalPrice - discountAmount;
+
+        Console.WriteLine($"Discount Amount: Rs. {discountAmount}");
+        Console.WriteLine($"Final Price: Rs. {finalPrice}");
+    }
+}
