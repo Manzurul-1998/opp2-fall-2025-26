308161d989a9a0d9418db3ae0764e831272154c8
17-35733-3 <81577924+Manzurul-1998@users.noreply.github.com>
2025-12-15 17:49:10 +0600

    Add files via upload

---
+++ lab/Student Record System/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+interface IStudent
+{
+    void DisplayInfo();
+}
+
+class Student : IStudent
+{
+    private string name;
+    private string id;
+    private double[] marks = new double[3];
+
+    public Student(string name, string id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    public double this[int index]
+    {
+        get { return marks[index]; }
+        set { marks[index] = value; }
+    }
+
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {name}, ID: {id}");
+        for (int i = 0; i < marks.Length; i++)
+        {
+            Console.WriteLine($"Mark {i}: {marks[i]}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student st = new Student("Akib", "22");
+        st[0] = 95.5;
+        st[1] = 90;
+        st[2] = 80;
+
+        st.DisplayInfo();
+    }
+
+}
